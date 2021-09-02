3b50e70d01fb691d6ee351aac68af0e41650dd4c
Rastislav Hepner <rastislav.hepner@gmail.com>
2021-09-02 14:25:04 +0200

    slideshow function initiates immediatelly

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 console.log('file has been loaded')
+
+let timeoutID = slideshow() 
+
+function slideshow() {
+    btns = document.querySelectorAll(".QTFfk")
+    if (!btns) {
+        console.log("Error: modal controls Next & Previous not found")
+    }
+    imgFullscreen()
+    return nextSlide()
+}
+
+function nextSlide() {
+    btns[1].click()
+    imgFullscreen()
+    return setTimeout(nextSlide(), 5000)
+}
+
+function imgFullscreen() {
+    document.querySelectorAll('.ReactModal__Content .oCCRx')[1].requestFullscreen())
+}
