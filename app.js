20a6675cd62f7c98a0878c7e41bedf3c0f3d276d
Rastislav Hepner <rastislav.hepner@gmail.com>
2021-09-02 16:53:43 +0200

    pagination fixed

--- app.js
+++ app.js
@@ -3,20 +3,22 @@ console.log('file has been loaded')
 let timeoutID = slideshow() 
 
 function slideshow() {
-    btns = document.querySelectorAll(".QTFfk")
+    var btns = document.querySelectorAll(".QTFfk")
     if (!btns) {
         console.log("Error: modal controls Next & Previous not found")
     }
-    imgFullscreen()
+    setImgFullscreen()
     return nextSlide()
 }
 
 function nextSlide() {
-    btns[1].click()
-    imgFullscreen()
+    for (btn of btns) {
+        btn.title === "Next" ? btn.click() : document.exitFullscreen() 
+    }
+    setImgFullscreen()
     return setTimeout(nextSlide(), 5000)
 }
 
-function imgFullscreen() {
+function setImgFullscreen() {
     document.querySelectorAll('.ReactModal__Content .oCCRx')[1].requestFullscreen()
 }
