fea248f6dc593f7df95e8407559e0eaa6ca7eabc
Rastislav Hepner <rastislav.hepner@gmail.com>
2021-09-03 12:49:02 +0200

    Complete rework

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 console.log('file has been loaded')
 
-let timeoutID = slideshow() 
+const slideTimeout = 5000
+
+slideshow()
 
 function slideshow() {
-    var btns = document.querySelectorAll(".QTFfk")
-    if (!btns) {
-        console.log("Error: modal controls Next & Previous not found")
-    }
+    // is this a modal? If yes play
+    updateSlide()
+}
+
+function updateSlide() {
     setImgFullscreen()
-    return nextSlide()
+    setTimeout(nextSlide, slideTimeout)
 }
 
 function nextSlide() {
+    // Get all navigation buttons of a modal
+    var btns = document.querySelectorAll(".QTFfk")
     for (btn of btns) {
-        btn.title === "Next" ? btn.click() : document.exitFullscreen() 
+        if (btn.title === "Next") {
+            btn.click()
+            updateSlide()
+            return
+        }
     }
-    setImgFullscreen()
-    return setTimeout(nextSlide(), 5000)
+    document.exitFullscreen()
 }
+
 
 function setImgFullscreen() {
     document.querySelectorAll('.ReactModal__Content .oCCRx')[1].requestFullscreen()
